68c367338e4e66c7fef28523b6634af5fedc97ba
pikagamed <51389229+pikagamed@users.noreply.github.com>
2019-06-26 16:29:55 +0800

    Update Practice05.cs
    練習跳躍、列舉、SWITCH

--- CSharpPractice05/Assets/Practice05.cs
+++ CSharpPractice05/Assets/Practice05.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//列舉
+//列舉為實質型別，宣告一組具名常數
+public enum Season
+{
+    Spring, Summer, Fall, Winter
+}
+
+public enum ItemPotion
+{
+    RedHerb, BlueHerb, Others
+}
+
 public class Practice05 : MonoBehaviour
 {
     public int A = 1, B = 1,C=1;
     public GameObject Wall;
     public int[] scores = { 10, 60, 55, 33, 1, 75, 98, 22, 24, 32 };
+
+    public ItemPotion prop = ItemPotion.RedHerb;
 
     void Start()
     {
@@ -84,6 +98,19 @@ public class Practice05 : MonoBehaviour
 
 
         //跳躍陳述式
+
+        //goto 中斷目前所在的迴圈陳述式，並跳到指定標記處繼續執行
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i == 5) goto Talk;
+
+            Debug.Log("<color=blue><b> goto數字" + i + "</b></color>");
+        }
+        Talk:
+            Debug.Log("<color=red>我在標記內</color>");
+            Debug.Log("<color=red>YO~</color>");
+
+
         //break 中斷目前所在的迴圈陳述式，並結束該迴圈
         for (int i = 1; i <=10; i++)
         {
@@ -93,7 +120,6 @@ public class Practice05 : MonoBehaviour
         }
         Debug.Log("<color=red>88行for迴圈陳述式下方敘述</color>");
 
-        //跳躍陳述式
         //continue 中斷目前所在的迴圈陳述式，但該迴圈繼續執行
         for (int i = 1; i <= 10; i++)
         {
@@ -102,11 +128,23 @@ public class Practice05 : MonoBehaviour
             Debug.Log("<color=blue><b> continue數字" + i + "</b></color>");
         }
         Debug.Log("<color=red>98行for迴圈陳述式下方敘述</color>");
+
     }
 
     // Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    void Update()
+    {
+        switch (prop)
+        {
+            case ItemPotion.RedHerb:
+                Debug.Log("回復HP <color=#008000><b>HP +10</b></color>");
+                break;
+            case ItemPotion.BlueHerb:
+                Debug.Log("回復HP <color=#0000FF><b>MP +10</b></color>");
+                break;
+            default:
+                Debug.Log("<color=#FF0000>這不是一個正常的道具</color>");
+                break;
+        }
+    }
 }
